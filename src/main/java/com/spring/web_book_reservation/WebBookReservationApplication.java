4a49731a31df6bb644e2505f7dbc07dc8ad84ec3
AndreaousN <a.nizevicius3@gmail.com>
2024-06-10 13:52:40 +0300

    initial commit

---
+++ src/main/java/com/spring/web_book_reservation/WebBookReservationApplication.java
@@ -0,0 +1,13 @@
+package com.spring.web_book_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBookReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBookReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/web_book_reservation/WebBookReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.web_book_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBookReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
